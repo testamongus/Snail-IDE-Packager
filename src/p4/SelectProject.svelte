f60735a8f61db5157f6b47f6bd5addf652de84cc
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-12-24 16:06:37 -0500

    .snail

--- src/p4/SelectProject.svelte
+++ src/p4/SelectProject.svelte
@@ -114,7 +114,7 @@
   };
   const handleDrop = ({detail: dataTransfer}) => {
     const name = dataTransfer.files[0].name;
-    if (name.endsWith('.sb') || name.endsWith('.sb2') || name.endsWith('.sb3') || name.endsWith('.pm') || name.endsWith('.pmp')) {
+    if (name.endsWith('.sb') || name.endsWith('.sb2') || name.endsWith('.sb3') || name.endsWith('.pm') || name.endsWith('.pmp') || name.endsWith('.snail')) {
       $type = 'file';
       setFiles(dataTransfer.files);
     }
@@ -246,7 +246,7 @@
           <input type="radio" name="project-type" bind:group={$type} value="file">
           {$_('select.file')}
         </label>
-        <input hidden={$type !== "file"} on:change={handleFileInputChange} bind:this={fileInputElement} type="file" accept=".sb,.sb2,.sb3, .pm, .pmp, .goobert">
+        <input hidden={$type !== "file"} on:change={handleFileInputChange} bind:this={fileInputElement} type="file" accept=".sb,.sb2,.sb3, .pm, .pmp, .snail, .goobert">
       </div>
       <div class="option">
         <label>
